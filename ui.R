b32b49fb432d300390881ae091897337f9bb2fff
Trentham3269 <scottdalwood@gmail.com>
2019-04-15 14:12:40 +1000

    Data update post VRA Queens

--- ui.R
+++ ui.R
@@ -71,7 +71,7 @@ shinyUI(
         br(),
         br(),
         
-        p("Data updated on 12 March 2019 and now includes 2019 TRA Championships"),
+        p("Data updated on 15 April 2019 and now includes 2019 VRA Championships"),
         
         a(href="https://trentham3269.shinyapps.io/shinyrangemap/", "NRAA Ranges")
       ),
