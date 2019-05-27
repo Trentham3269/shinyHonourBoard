c49246d7440136d81f7371fae2c313c186a7a6e4
Trentham3269 <scottdalwood@gmail.com>
2019-05-27 23:31:49 +1000

    Update data for North Queensland

--- ui.R
+++ ui.R
@@ -71,7 +71,7 @@ shinyUI(
         br(),
         br(),
         
-        p("Data updated on 15 April 2019 and now includes 2019 VRA Championships"),
+        p("Data updated on 27 May 2019 and now includes 2019 NQRA Championships"),
         
         a(href="https://trentham3269.shinyapps.io/shinyrangemap/", "NRAA Ranges")
       ),
