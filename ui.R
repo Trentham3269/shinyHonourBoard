574b00279a337554eabf388f8e371a3c556f208e
Trentham3269 <scottdalwood@gmail.com>
2019-09-08 20:22:57 +1000

    Update data for SA and QLD championships

--- ui.R
+++ ui.R
@@ -71,7 +71,7 @@ shinyUI(
         br(),
         br(),
         
-        p("Data updated on 23 June 2019 and now includes 2019 NRAA Championships"),
+        p("Data updated on 8 September 2019 and now includes 2019 QRA Championships"),
         
         a(href="https://trentham3269.shinyapps.io/shinyrangemap/", "NRAA Ranges")
       ),
