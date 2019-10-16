9308ea6ba965df5d8929c55ff18f043350ff67a7
Trentham3269 <scottdalwood@gmail.com>
2019-10-16 16:33:20 +1100

    Data update post NSWRA

--- ui.R
+++ ui.R
@@ -71,7 +71,7 @@ shinyUI(
         br(),
         br(),
         
-        p("Data updated on 8 September 2019 and now includes 2019 QRA Championships"),
+        p("Data updated on 16 October 2019 and now includes 2019 NSWRA Championships"),
         
         a(href="https://trentham3269.shinyapps.io/shinyrangemap/", "NRAA Ranges")
       ),
