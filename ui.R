a5b81b3e65aedd86b167a99f1264e86856f3dfa4
Trentham3269 <scottdalwood@gmail.com>
2020-02-04 23:08:16 +1100

    Update data after NZ 2020

--- ui.R
+++ ui.R
@@ -71,7 +71,7 @@ shinyUI(
         br(),
         br(),
         
-        p("Data updated on 24 November 2019 and now includes 2019 ACT Championships"),
+        p("Data updated on 4 February 2020 and now includes 2020 NZ Championships"),
         
         a(href="https://trentham3269.shinyapps.io/shinyrangemap/", "NRAA Ranges")
       ),
